f165de033022a51c3cfadf6141e1ab3b787033b5
Kiran Bose <kiranbose@gmail.com>
2021-09-09 11:54:40 +0530

    added pwa

--- android_app/polyfills.js
+++ android_app/polyfills.js
@@ -9868,8 +9868,8 @@ var __importDefault = (undefined && undefined.__importDefault) || function (mod)
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Volumes/store 1/code/Audit-UI-Angular/src/polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! /Volumes/store 1/code/Audit-UI-Angular/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! C:\git code\Audit-UI-Angular\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\git code\Audit-UI-Angular\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
